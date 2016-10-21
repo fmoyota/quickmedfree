84d11b0ff9a6edfa4af451e890b260f37d77ce60
Fernando Moyota <fmoyota@FMOYOTA-MacBook-Pro.local>
2016-10-20 21:36:04 -0500

    Prueba QuickMed Free v1.0.0

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// JavaScript Document
+
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+var checkDevice = function checkDevice(){
+	var jqxhr = $.getJSON( "https://www.edifarm.com.ec/edifarm_quickmed/ws/mobile/login.php",
+				{
+					deviceuuid:device.uuid, 
+					devicename:device.manufacturer, 
+					version:device.version, 
+					platform:device.platform, 
+					model:device.model,
+					action:'consultar',
+					
+				}, function() {
+					console.log('CheckDevice');
+				})
+				  .done(function(data) {
+					var r=data.respuesta;
+					if(r==='1'){
+						window.location='home.html';	
+					}
+					
+				  })
+				  .fail(function() {
+					
+				  })
+				  .always(function() {
+					$('.loader').fadeOut('slow');
+				});	
+};
