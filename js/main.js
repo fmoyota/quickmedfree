e379040219ccb1ba34f30848a3c78f106d5ea8f6
Fernando Moyota <fmoyota@gmail.com>
2016-11-23 00:13:26 -0500

    Update Styles

--- js/main.js
+++ js/main.js
@@ -10,170 +10,174 @@ device={
 //*/
 
 var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+	var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+		sURLVariables = sPageURL.split('&'),
+		sParameterName,
+		i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+	for (i = 0; i < sURLVariables.length; i++) {
+		sParameterName = sURLVariables[i].split('=');
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
-    }
+		if (sParameterName[0] === sParam) {
+			return sParameterName[1] === undefined ? true : sParameterName[1];
+		}
+	}
 };
 
-var checkDevice = function checkDevice(){
-	
-	var jqxhr = $.getJSON( "https://quickmed.edifarm.com.ec/ws/mobile/login.php",
-				{
-					deviceuuid:device.uuid, 
-					devicename:device.manufacturer, 
-					version:device.version, 
-					platform:device.platform, 
-					model:device.model,
-					action:'consultar',
-					
-				}, function() {
-					console.log('CheckDevice');
-				})
-				  .done(function(data) {
-					var r=data.respuesta;
-					if(r==='1'){
-						window.location='home.html';	
-					}
-					
-				  })
-				  .fail(function() {
-					
-				  })
-				  .always(function() {
-					$('.loader').fadeOut('slow');
-				});	
+var checkDevice = function checkDevice() {
+
+	var jqxhr = $.getJSON("https://quickmed.edifarm.com.ec/ws/mobile/login.php", {
+			deviceuuid: device.uuid,
+			devicename: device.manufacturer,
+			version: device.version,
+			platform: device.platform,
+			model: device.model,
+			action: 'consultar',
+
+		}, function () {
+			console.log('CheckDevice');
+		})
+		.done(function (data) {
+			var r = data.respuesta;
+			if (r === '1') {
+				window.location = 'home.html';
+			}
+
+		})
+		.fail(function () {
+
+		})
+		.always(function () {
+			$('.loader').fadeOut('slow');
+		});
 };
 
 
-var getSettings = function getSettings(){
+var getSettings = function getSettings() {
 	"use strict";
+alert('Settings');
+	var jqxhr = $.getJSON("https://quickmed.edifarm.com.ec/ws/mobile/login.php", {
+				deviceuuid: device.uuid,
+				devicename: device.manufacturer,
+				version: device.version,
+				platform: device.platform,
+				model: device.model,
+				action: 'consultar',
+			},
+			function () {
+				
+				alert('Get User Data');
+			})
+		.done(function (data) {
+			
+				alert('Done ' +JSON.stringify(data));
+	/*		
+			var r = data.respuesta;
+			var e = '';
+			var u = '';
+			var not = '0';
+			var med = '0';
+			alert(JSON.stringify(data));
+			console.log('done: ' + JSON.stringify(data));
+
+			if (r === '1') {
+				
+
+				u = data.datos.email;
+				e = data.datos.nombre_usuario;
+				if (u !== e) {
+					$('#email').removeAttr('readonly');
+				}
+
+				$('#email').val(data.datos.email);
+				$('#nombres').val(data.datos.fname);
+				$('#apellidos').val(data.datos.lname);
+				$('#telefono').val(data.datos.phone);
+				$("#pais option").filter(function () {
+					return $(this).text() === data.datos.country;
+				}).prop('selected', true);
+				$('#ciudad').val(data.datos.city);
+
+				$('#cumple').val(data.datos.birth);
+				not = data.datos.notification;
+				if (not === '1') {
+					$('#cmn-toggle-1').attr('checked', true);
+				} else {
+					$('#cmn-toggle-1').attr('checked', false);
+				}
+				med = data.datos.ismedic;
+				if (med === '1') {
+					$('#ismedic').attr('checked', true);
+				} else {
+					$('#ismedic').attr('checked', false);
+				}
+
+				
+			} else {
+				console.log('send form new user');
+
+				window.location = 'home.html';
+			}
+
+			alert('fin');
+			$('.loader').fadeOut('slow');
+//*/
+		})
+		.fail(function (data) {
+
+			//console.log('Error' + JSON.stringify(data));
+			alert('Error' + JSON.stringify(data));
+		})
+		.always(function () {
+			console.log('Obtener Settings');
+			alert('FIN');
+			$('.loader').fadeOut('slow');
+		});
 	
-	var jqxhr = $.getJSON( "https://quickmed.edifarm.com.ec/ws/mobile/login.php",
-				{
-					deviceuuid: device.uuid,
-					devicename: device.manufacturer,
-					version: device.version,
-					platform: device.platform,
-					model: device.model,
-					action: 'consultar',
-
-				},
-				function () {
-					console.log('Get User Data');
-				})
-				.done(function (data) {
-						var r = data.respuesta;
-						var e = '';
-						var u = '';
-						var not = '0';
-						var med = '0';
-						alert(JSON.stringify(data));
-						console.log('done: ' + JSON.stringify(data));
-
-						if (r === '1') {
-							/* 
-
-													u=data.datos.email;
-													e=data.datos.nombre_usuario;
-													if(u!==e){
-														$('#email').removeAttr('readonly');
-													}
-
-													$('#email').val(data.datos.email);
-													$('#nombres').val(data.datos.fname);
-													$('#apellidos').val(data.datos.lname);
-													$('#telefono').val(data.datos.phone);
-													$("#pais option").filter(function() {
-														return $(this).text() === data.datos.country; 
-													}).prop('selected', true);
-													$('#ciudad').val(data.datos.city);
-
-													$('#cumple').val(data.datos.birth);
-													not=data.datos.notification;
-													if(not==='1'){
-														$('#cmn-toggle-1').attr('checked',true);
-													}else{
-														$('#cmn-toggle-1').attr('checked',false);
-													}
-													med=data.datos.ismedic;
-													if(med==='1'){
-														$('#ismedic').attr('checked',true);
-													}else{
-														$('#ismedic').attr('checked',false);
-													}
-
-												*/
-						} else {
-							console.log('send form new user');
-
-							window.location = 'home.html';
-						}
-
-						alert('fin');
-						$('.loader').fadeOut('slow');
-
-					})
-					.fail(function (data) {
-
-						console.log('Error' + JSON.stringify(data));
-						alert('Error' + JSON.stringify(data));
-					})
-					.always(function () {
-						console.log('Obtener Settings');
-						alert('FIN');
-						$('.loader').fadeOut('slow');
-					});
+			$('.loader').fadeOut('slow');
 };
 
 
-function admobDisplay(){
-	
-	var banner='ca-app-pub-7271854751013605/3853227419';
-	var interlestial='ca-app-pub-7271854751013605/5469561410';
-	
-	
-      document.removeEventListener('deviceready', admobDisplay, false);
-			 // Set AdMobAds options:
-    
-	switch (device.platform){
+function admobDisplay() {
+
+	var banner = 'ca-app-pub-7271854751013605/3853227419';
+	var interlestial = 'ca-app-pub-7271854751013605/5469561410';
+
+
+	document.removeEventListener('deviceready', admobDisplay, false);
+	// Set AdMobAds options:
+
+	switch (device.platform) {
 		case 'iOS':
-			banner='ca-app-pub-7271854751013605/3853227419';
-			interlestial='ca-app-pub-7271854751013605/5469561410';
+			banner = 'ca-app-pub-7271854751013605/3853227419';
+			interlestial = 'ca-app-pub-7271854751013605/5469561410';
 			break;
-		
+
 		case 'Android':
-			banner='ca-app-pub-7271854751013605/9457279011';
-			interlestial='ca-app-pub-7271854751013605/1934012219';
+			banner = 'ca-app-pub-7271854751013605/9457279011';
+			interlestial = 'ca-app-pub-7271854751013605/1934012219';
 			break;
 	}
-	
-	
+
+
 	admob.setOptions({
-        publisherId:  banner,  // Required
-        interstitialAdId: interlestial,  // Optional
-    });
-	
-	
+		publisherId: banner, // Required
+		interstitialAdId: interlestial, // Optional
+	});
+
+
 	admob.createBannerView();
 
-	  // Request interstitial (will present automatically when autoShowInterstitial is set to true)
-	admob.requestInterstitial();	
+	// Request interstitial (will present automatically when autoShowInterstitial is set to true)
+	admob.requestInterstitial();
 }
 
 
-function successAnalytics(){
+function successAnalytics() {
 	console.log('Funciona perfecto');
 }
-function failAnalytics(){
+
+function failAnalytics() {
 	consolel.log('No se conecta analytics');
 }
 
