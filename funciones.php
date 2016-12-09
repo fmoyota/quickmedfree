8a59225e3ed97932712f60bf592ac57d13af4cc5
Fernando Moyota <fmoyota@gmail.com>
2016-12-09 16:37:14 -0500

    Update settings

--- funciones.php
+++ funciones.php
@@ -6,15 +6,15 @@ function dispositivo($line){
 		$line['date']=@date('Y-m-d H:i:s');
 	
 	// archivo plano
-        $new_contents="device=".json_encode($line)." \t\n";
+        $new_contents="var device=".json_encode($line)." \t\n";
         $fh = @fopen($file, 'w+'); // si no existe el archivo lo crea de modo que escriba al final del archivo sin eliminar los datos existentes
         @fwrite($fh, $new_contents);
 // escribe la linea en el archivo
         @fclose($fh); //cierra el archivo
 }
 
-
-dispositivo(@$_REQUEST['dispositivo']);
+//$dispositivo=json_encode($_REQUEST);
+dispositivo($_REQUEST);
 
 
 ?>
